36431cea2797463ed9d8c116a012d72655352fc1
ishuen <12228644+ishuen@users.noreply.github.com>
2021-10-11 21:32:36 +0800

    db prep

---
+++ src/main/java/TradeEntity.java
@@ -0,0 +1,10 @@
+import java.sql.Timestamp;
+
+public class TradeEntity {
+  private String symbol;
+  private long id;
+  private String exchangeCode;
+  private double price;
+  private int quantity;
+  private Timestamp updateTime;
+}
